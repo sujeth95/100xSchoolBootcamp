e5bb07bd0d6221853a1537943ce794fd112201b0
sujeth95 <sujeetoraon337@gmail.com>
2026-01-29 15:34:13 +0530

    Few Corrections of Assignment

--- Web_Dev/index.js
+++ Web_Dev/index.js
@@ -1,45 +1,45 @@
 //-------- Variable Assignment 1: --------
-// let color = "Red";
-// const height = "173 cm";
-// var likePizza = true;
+let color = "Red";
+const height = "173 cm";
+var likePizza = true;
 
-// console.log(color);
-// console.log(height);
-// console.log(likePizza);
+console.log(color);
+console.log(height);
+console.log(likePizza);
 
 //--------- Functions Assignment 1: ---------------
-// function sum(n, m){
-//     let sumAns = n + m;
+function sum(n, m){
+    let sumAns = n + m;
 
-//     return sumAns;
-// }
+    return sumAns;
+}
 
-// let ans = sum(1,2);
-// // let ans = sum("1","2"); // Passing string instead of number.
-// console.log(ans);
+let ans = sum(1,2);
+// let ans = sum("1","2"); // Passing string instead of number.
+console.log(ans);
 
 // ---------- Functions Assignment 2: ---------------
-// function canVote(n) {
+function canVote(n) {
 
-//     if (n >= 18) {
-//         console.log("Can Vote");
-//     } else {
-//         console.log("You are still a child");
-//     }
+    if (n >= 18) {
+        console.log("Can Vote");
+    } else {
+        console.log("You are still a child");
+    }
 
-//     return;
-// }
+    return;
+}
 
-// let vote = canVote(100);
+let vote = canVote(100);
 
 // ------------ If/Else Assignment -----------------------
-// let n = 26;
+let n = 26;
 
-// if (n % 2 == 0) {
-//     console.log("The number is even.");
-// } else {
-//     console.log("The number is odd.");
-// }
+if (n % 2 == 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
 
 // -------------- Loops Assignment ------------------------
 function sum(n){
