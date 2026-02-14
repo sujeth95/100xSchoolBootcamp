07e2c026cf55aef0286cd70a0f944988936613c0
sujeth95 <sujeetoraon337@gmail.com>
2026-02-14 08:35:15 +0530

    Assignments

--- Web_Dev/Day2/Assignments/js-basics/ObjectArrayBeginner.js
+++ Web_Dev/Day2/Assignments/js-basics/ObjectArrayBeginner.js
@@ -1,25 +1,47 @@
 // Question 1:
-const input = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
-const output = {};
+// const input = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
+// const output = {};
 
-const categories = Object.keys(input);  // ["food", "travel", "bills"]
-let i = 0;  // start with the first category
+// const categories = Object.keys(input);  // ["food", "travel", "bills"]
+// let i = 0;  // start with the first category
 
-while (i < categories.length) {
-  const category = categories[i];   // pick the current category
-  const numbers = input[category];  // get the list of numbers
-  let total = 0;
+// while (i < categories.length) {
+//   const category = categories[i];   // pick the current category
+//   const numbers = input[category];  // get the list of numbers
+//   let total = 0;
 
-  let j = 0;  // start with the first number
-  while (j < numbers.length) {
-    total = total + numbers[j];  // add the number to total
-    j = j + 1;                   // move to the next number
-  }
+//   let j = 0;  // start with the first number
+//   while (j < numbers.length) {
+//     total = total + numbers[j];  // add the number to total
+//     j = j + 1;                   // move to the next number
+//   }
 
-  output[category] = total;  // save the total for this category
-  i = i + 1;                 // move to the next category
-}
+//   output[category] = total;  // save the total for this category
+//   i = i + 1;                 // move to the next category
+// }
 
-console.log(output);
+// console.log(output);
 // { food: 60, travel: 20, bills: 100 }
 
+
+//--------- Question 2 --------------------------------
+
+// let input = ["apple", "banana", "apple", "orange", "banana", "apple"];
+// const count = {};
+
+// let i = 0;
+// for (i = 0; i < input.length; i++) {
+//   let word = input[i];
+//   count[word] = (count[word] || 0) + 1;
+// }
+// console.log(count);
+
+// Effiecient way of writing the above:
+const input = ["apple", "banana", "apple", "orange", "banana", "apple"];
+const count = {};
+
+for(let word of input) {
+  count[word] = (count[word] || 0) + 1;
+}
+
+console.log(count);
--- Web_Dev/Day2/Assignments/js-basics/Practice.js
+++ Web_Dev/Day2/Assignments/js-basics/Practice.js
@@ -1,21 +1,10 @@
-const input = { food: [10, 20, 30], travel: [5, 15], bills: [40, 60] };
+const input = ["apple", "banana", "apple", "orange", "banana", "apple"];
 const output = {};
 
-const categories = Object.keys(input);
 let i = 0;
-
-while (i < categories.length) {
-    const category = categories[i];
-    const numbers = input[category];
-    let total = 0;
-
-    let j = 0;
-    while (j < numbers.length) {
-        total = total + numbers[j];
-        j = j + 1;
-    }
-    output[category] = total;
-    i = i + 1;
+for (i; i < input.length; i++) {
+    let word = input[i];
+    output[word] = (output[word] || 0) + 1;
 }
 
-console.log(output);    
+console.log(output);
