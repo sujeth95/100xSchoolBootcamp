73593ce0dbe9758d4e624ca46d9c8a8aa14d2422
sujeth95 <sujeetoraon337@gmail.com>
2026-02-24 09:04:08 +0530

    Modifying Folders Name

--- Web_Dev/Day 3/stopWatch.js
+++
@@ -1,8 +0,0 @@
-// Creating stopwatch
-let ctr = 0;
-function callback() {
-    console.log(ctr);
-    ctr = ctr + 1;
-}
-
-setInterval(callback, 1000);
--- Web_Dev/Day 4/index3.js
+++
@@ -1,5 +0,0 @@
-const d = new Date();
-console.log(d.getDay());
-console.log(d.getMonth());
-console.log(d.getFullYear());
-console.log(d.getDate());
