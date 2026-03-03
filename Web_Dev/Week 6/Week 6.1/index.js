f5665de1755c62dc50ce974e95603a5079d624b2
sujeth95 <sujeetoraon337@gmail.com>
2026-03-03 23:43:28 +0530

    Week 6 Offline Class Few Notes

---
+++ Web_Dev/Week 6/Week 6.1/index.js
@@ -0,0 +1,6 @@
+// const chalk = require("chalk");
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello, world'));
+console.log(chalk.red.bold('This is an error message.'));
+console.log(chalk.green.underline('This is a success message.'));
---
+++ Web_Dev/Week 6/Week 6.1/index2.js
@@ -0,0 +1,4 @@
+const path = require("path");
+console.log(__dirname);
+console.log(__dirname + "/index.js");
+console.log(path.join(__dirname, "index.js"));
