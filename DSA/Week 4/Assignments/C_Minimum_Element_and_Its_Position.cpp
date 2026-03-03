ef9ab70b02ffbb8220fc06e464f5102517aa5183
sujeth95 <sujeetoraon337@gmail.com>
2026-03-03 12:33:05 +0530

    DSA Assignments

---
+++ DSA/Week 4/Assignments/C_Minimum_Element_and_Its_Position.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    long long ans = a[0];
+    int locus = 1;
+
+    for (int i = 1; i < n; i++)
+    { 
+        if(a[i] < ans){
+            ans = a[i];
+            locus = i + 1;
+        }
+    }
+
+    cout<<ans<<" "<<locus;
+
+    return 0;
+}
---
+++ DSA/Week 4/Assignments/index.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[10];
+
+    for (int i = 0; i <= 9; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i <= 9; i++)
+    {
+        cout << a[i];
+    }
+
+    return 0;
+}
