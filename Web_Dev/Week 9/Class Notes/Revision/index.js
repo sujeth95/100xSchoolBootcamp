74d40cb26c9101080d1815a8da402661580d7987
sujeth95 <sujeetoraon337@gmail.com>
2026-04-21 15:52:43 +0530

    Writing backend of Notes application

--- Web_Dev/Week 9/Class Notes/Revision/index.js
+++ Web_Dev/Week 9/Class Notes/Revision/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 
 app.use(express.json());
@@ -24,6 +26,8 @@ app.post("/signup", function (req, res) {
     res.json({
         message: "You have signed up"
     })
+
+    console.log(users);
 })
 
 app.post("/signin", function (req, res) {
@@ -38,29 +42,84 @@ app.post("/signin", function (req, res) {
         return;
     }
 
-    
-
+    const token = jwt.sign({
+        username
+    }, "sujeet123");
+    res.json({
+        token: token
+    })
+    console.log(token);
 })
 
-// POST - Create a note
+// POST - Create a note -- AUTHENTICATED ENDPOINT
 app.post("/notes", function (req, res) {
+    // Check if they have sent the right header, extract who this user is from the header.
+    const token = req.headers.token;
+
+    if (!token) {
+        res.status(403).send({
+            message: "You are not logged in"
+        });
+        return;
+    }
+
+    const decoded = jwt.verify(token, "sujeet123");
+    const username = decoded.username;
+
+    if (!username) {
+        res.status(403).json({
+            message: "malformed token"
+        })
+        return;
+    }
+
     const note = req.body.note;
-    notes.push(note);
+    notes.push({ note, username });
 
     res.json({
         message: "Done!"
     })
+
+    console.log(notes);
 })
 
-// GET - get all my notes
+// GET - get all my notes -- AUTHENTICATED ENDPOINT
 app.get("/notes", function (req, res) {
-    res.send({
-        notes
+    const token = req.headers.token;
+
+    if (!token) {
+        res.status(403).send({
+            message: "You are not logged in"
+        })
+        return;
+    }
+
+    const decoded = jwt.sign(token, "sujeet123");
+    const username = decoded.username;
+
+    if (!username) {
+        res.send(403).json({
+            message: "token invalid"
+        })
+        return;
+    }
+
+    const userNotes = notes.filter(note => note.username === username);
+    res.json({
+        notes: userNotes
     })
 })
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/frontend/index.html")
+})
+
+app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + "/frontend/signup.html");
+})
+
+app.get("/signin", (req,res) => {
+    res.sendFile(__dirname + "/frontend/signin.html");
 })
 
 const PORT = 3000;
