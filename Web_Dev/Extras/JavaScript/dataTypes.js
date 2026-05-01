b352418cdb7a88325c9d537eca4185866d43650a
sujeth95 <sujeetoraon337@gmail.com>
2026-05-01 14:49:29 +0530

    Learning JS Basics from YT

---
+++ Web_Dev/Extras/JavaScript/dataTypes.js
@@ -0,0 +1,17 @@
+// "use strict"; // treat all JS code as newer version
+
+// alert("hello");   //we are using node.js, not a browser.
+
+// NOTE:
+// number  => 2 to power 53
+// bigint
+// String => ""
+// boolean => true / false
+// null => standalone value or empty value
+// undefined => 
+// symbol => unique
+
+// object
+// typeof => to find the type of data.
+// NOTE: Null is an object and have value 0. (IMPORTANT)
+// NOTE: Type of NaN is a number (IMPORTANT)
