93797e2f8eb486ea164e4f638e5baaf519427e6d
sujeth95 <sujeetoraon337@gmail.com>
2026-06-10 14:54:54 +0530

    Learning about SQL

--- Web_Dev/Week 13/index.js
+++ Web_Dev/Week 13/index.js
@@ -1,5 +1,27 @@
-const express = require("express");
+const express = require('express');
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString
+    connectionString: "postgresql://neondb_owner:npg_cWZk9Rx1viqj@ep-winter-breeze-aqqcbcna.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log("INSERT INTO users (username, email, password) VALUES ('" + username + "','" + password + "','" + email + "')");
+    await pool.query("INSERT INTO users (username, email, password) VALUES ('" + username + "','" + password + "','" + email + "')");
+        
+    res.json({
+        message: "Signup done"
+    })
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
 })
