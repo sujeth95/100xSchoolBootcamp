dd19c65fac97662ec32f3601387ecb9795d29231
sujeth95 <sujeetoraon337@gmail.com>
2026-06-24 22:17:30 +0530

    React

--- Web_Dev/Week 18/Warm_Up/ReactBasicPartI/src/App.jsx
+++ Web_Dev/Week 18/Warm_Up/ReactBasicPartI/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+
 function App() {
 
   return (
@@ -11,7 +12,10 @@ function App() {
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
           <div>
-            <PostComponents />
+            <PostComponents 
+            name={"harkirat"}
+            sub
+            />
             <br></br>
           </div>
           <div>
@@ -33,7 +37,7 @@ function App() {
 
 const style = { width: 300, backgroundColor: "white", borderRadius: 10, borderColor: "grey", borderWidth: 1, padding: 30 }
 
-function PostComponents() {
+function PostComponents({ name, followerCount, time, image, description }) {
   return <div style={style}>
     <div style={{ display: "flex" }}>
       <img src={"https://i.pinimg.com/originals/16/f3/15/16f3158eab90cca32a1f735566c9eb22.jpg"} style={{
@@ -45,12 +49,16 @@ function PostComponents() {
         <b>
           100xdevs
         </b>
-        <div>23,888 followers</div>
-        <div>12m</div>
+        <div>200 followers</div>
+        <div style={{ display: "flex" }}>
+          <div>1 day ago</div>
+          <img src={"https://img.magnific.com/premium-vector/round-wall-clock-cartoon_119631-491.jpg?semt=ais_hybrid&w=740&q=80"} style={{ width: 20, height: 20, marginLeft: 5 }} />
+        </div>
+        <div>{time}</div>
       </div>
     </div>
     <div style={{ fontSize: 18 }}>
-      What to know how to win big? Check out how these folks won $6000 in bounties.
+      {description}
     </div>
   </div>
 }
