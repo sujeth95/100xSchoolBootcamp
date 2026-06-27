f7a76f6b678dbbf452b948f3e7e4ffc36391c682
sujeth95 <sujeetoraon337@gmail.com>
2026-06-27 12:24:09 +0530

    Revisting React

--- Web_Dev/Week 18/Warm_Up/ReactBasicPartI/src/App.jsx
+++ Web_Dev/Week 18/Warm_Up/ReactBasicPartI/src/App.jsx
@@ -9,21 +9,26 @@ function App() {
 
   return (
     <div style={{ height: "100vh", background: "#dfe6e9" }}>
+      <ToggleMessage />
       <div style={{ display: "flex", justifyContent: "center" }}>
         <div>
           <div>
             <PostComponents 
             name={"harkirat"}
-            sub
+            subtitle={"20 followers"}
+            time={"2m ago"}
+            image={"https://i.pinimg.com/originals/16/f3/15/16f3158eab90cca32a1f735566c9eb22.jpg"}
+            description={"What to know how to win big? Check out how these folks won $6000 in bounties."}
             />
             <br></br>
           </div>
           <div>
-            <PostComponents />
-            <br></br>
-          </div>
-          <div>
-            <PostComponents />
+            <PostComponents 
+            name={"raman"}
+            subtitle={"Promoted"}
+            image={"https://i.pinimg.com/originals/16/f3/15/16f3158eab90cca32a1f735566c9eb22.jpg"}
+            description={"How to get hired in 2026? I lost my job in 2025, this is the roadmap I followed to get a job"}
+            />
             <br></br>
           </div>
         </div>
@@ -37,24 +42,37 @@ function App() {
 
 const style = { width: 300, backgroundColor: "white", borderRadius: 10, borderColor: "grey", borderWidth: 1, padding: 30 }
 
-function PostComponents({ name, followerCount, time, image, description }) {
+const ToggleMessage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setIsVisible(!isVisible)}>
+        Toggle Message
+      </button>
+      {isVisible && <p>This message is conditionally rendered!</p>}
+    </div>
+  );
+};
+
+function PostComponents({ name, subtitle, time, image, description }) {
   return <div style={style}>
     <div style={{ display: "flex" }}>
-      <img src={"https://i.pinimg.com/originals/16/f3/15/16f3158eab90cca32a1f735566c9eb22.jpg"} style={{
+      <img src={image} style={{
         width: 50,
         height: 50,
         borderRadius: 50
       }} />
       <div style={{ fontSize: 15, marginLeft: 15 }}>
         <b>
-          100xdevs
+          {name}
         </b>
-        <div>200 followers</div>
-        <div style={{ display: "flex" }}>
-          <div>1 day ago</div>
+        <div>{subtitle}</div>
+        {/* Adding condition for time */}
+        {(time !== undefined) ? <div style={{ display: "flex" }}>
+          <div>{time}</div>
           <img src={"https://img.magnific.com/premium-vector/round-wall-clock-cartoon_119631-491.jpg?semt=ais_hybrid&w=740&q=80"} style={{ width: 20, height: 20, marginLeft: 5 }} />
-        </div>
-        <div>{time}</div>
+        </div> : null}
       </div>
     </div>
     <div style={{ fontSize: 18 }}>
