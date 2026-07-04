c86169f52b09f4c341d1129b028d7bc80e656fec
sujeth95 <sujeetoraon337@gmail.com>
2026-07-04 17:39:00 +0530

    React Basics

--- Web_Dev/Week 19/React-Basics/src/App.tsx
+++ Web_Dev/Week 19/React-Basics/src/App.tsx
@@ -1,11 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+// NOTE: React only re-renders components if a "state variable" changes.
 function App() {
+  let [data, setData] = useState([]);
+
+  // if the dependency array (2nd argument) is empty
+  // this function only runs on the first render (mount)
+  useEffect(function () {
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+      .then(response => {
+        setData(response.data);
+      })
+  }, []);
+
+  // axios.get("https://jsonplaceholder.typicode.com/todos")
+  //   .then(response => {
+  //     setData(response.data);    //Changing a state variable.
+  //     // data = response.data;   //Changing a local variable.
+  //   })
 
   return <div>
-    <LinkedinPost backgroundColor={"red"} name={"Sujeet"} contents={"This is me!"}/>
-    <LinkedinPost backgroundColor={"green"} name={"Oraon"} contents={"Hii there!"}/>
-    <LinkedinPost backgroundColor={"orange"}/>
-    <LinkedinPost />
+    {data.map(todo => <Todo title={todo.title} />)}   //Converting array of objects into array of components
   </div>
 }
 
@@ -13,16 +29,12 @@ function App() {
 // components 
 // NOTE: functions takes arguments as an input
 // components takes (props => Props is nothing but an object) as an input
-function LinkedinPost(props: any) {
+function Todo(props: any) {
 
-  return <div style={{margin: 10, padding: 20, border: "1px solid black", borderRadius: 10, backgroundColor: props.backgroundColor }}>
+  return <div style={{ margin: 10, padding: 20, border: "1px solid black", borderRadius: 10, backgroundColor: props.backgroundColor }}>
     <div>
-      <b>{props.name}</b>
+      {props.title}
     </div>
-    <div>
-      {props.contents}
-    </div>
-
   </div>
 }
 
