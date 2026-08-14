4594ab521cc5e1e44dc65347d7d5100ba720a415
sujeth95 <sujeetoraon337@gmail.com>
2026-08-14 15:57:59 +0530

    Learning typescript

--- Web_Dev/Extras/TypeScript 2/src/index.ts
+++ Web_Dev/Extras/TypeScript 2/src/index.ts
@@ -17,28 +17,101 @@
 
 // USING INTERFACE...........
 
-interface Address {
-    city: string;
-    country: string;
-    pincode: number;
-    houseNumber?: string;
-}
+// interface Address {
+//     city: string;
+//     country: string;
+//     pincode: number;
+//     houseNumber?: string;
+// }
+// interface User {
+//     name: string,
+//     age: number,
+//     address: Address
+// }
+
+// let user: User = {
+//     name: "sujeet",
+//     age: 21,
+//     address: {
+//         city: "Ranchi",
+//         country: "India",
+//         pincode: 1002483
+//     }
+// }
+
+// function isLegal(user: User): boolean {
+//     return user.age >= 18
+// }
+
+
+// Interfaces as a class......
+
+// let person: People = {
+//     name: "sujeet",
+//     age: 21,
+    // greet: () => {
+    //     return "hi"
+    // }
+// }
+
+// let greeting = person.greet();
+// console.log(greeting);
+
 interface User {
     name: string,
     age: number,
-    address: Address
+    isLegal(): boolean
+    // greet: () => string,     // greet is a function
+    // greet2(): string
+    // greet(phrase: string): void  //greet is a function
 }
 
-let user: User = {
-    name: "sujeet",
-    age: 21,
-    address: {
-        city: "Ranchi",
-        country: "India",
-        pincode: 1002483
+class Manager implements User {
+    // name: string;
+    // age: number;
+
+    constructor(public name: string, public age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    isLegal() {
+        return this.age > 18
     }
 }
 
-function isLegal(user: User): boolean {
-    return user.age >= 18
-}
+let m = new Manager("John", 30);
+console.log(m.age)
+console.log(m.isLegal());
+
+// LEARNING ABOUT EXTENDS IN CLASS
+
+// class Shape{
+//     area() {
+//         console.log("Hii i am area");
+//     }
+// }
+
+// class Rectangle extends Shape{
+//     width: number;
+//     height: number;
+
+//     constructor() {
+//         super()         //important when using extends in class
+//         this.width = 1;
+//         this.height = 2;
+//     }
+// }
+
+
+
+// ABSTRACT CLASSES...........
+
+// abstract class User {
+//     // name: string
+//     constructor(name: string) {
+//         this.name = name
+//     }
+
+//     abstract greet: () => string;
+// }
